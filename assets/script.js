9f2854ce363691d5c8841f2d65b0760ee8e2c883
Mickael <mickaelmarchand@protonmail.com>
2024-11-13 13:59:53 +0100

    update

--- assets/script.js
+++ assets/script.js
@@ -3,6 +3,8 @@ const ctx = canvas.getContext("2d");
 
 const image = new Image();
 image.onload = function () {
+  canvas.width = this.naturalWidth;
+  canvas.height = this.naturalHeight;
   ctx.drawImage(image, 0, 0, canvas.width, canvas.height);
 };
 
