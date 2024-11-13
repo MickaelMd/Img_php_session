a2b2091f81355b5b69e38e6b146090ff9af95734
Mickael <mickaelmarchand@protonmail.com>
2024-11-13 11:33:08 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,52 @@
+<?php require "script.php";?>
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles.css">
+    <title>Document</title>
+</head>
+
+<body>
+    <section id="main_section">
+
+        <img id="img" src="<?=$img?>" />
+        <form action="" method="POST" enctype="multipart/form-data">
+            <input id="img_btn" type="file" name="img_session" required>
+            <button type="submit" name="load">Charger l'image</button>
+        </form>
+
+        <form action="" method="POST">
+
+            <button type="submit" name="reset">Reset</button>
+            <button type="submit" name="refresh">Recharger la page</button>
+        </form>
+    </section>
+
+    <section id="info">
+
+        <p>$inactive = 256; </br>
+            ini_set('session.gc_maxlifetime', $inactive);</br> </br>
+
+            <span class="comment"> Définis un temps en secondes </span> </br> </br>
+
+            session_start();</br>
+
+            if (isset($_SESSION['time_session']) && (time() - $_SESSION['time_session'] > $inactive)) {</br> </br>
+            <span class="comment"> Regarde l'heure de la dernière requête et si c'est plus que $inactive, alors on
+                détruit
+                la session </span> </br> </br>
+            session_unset(); </br>
+            session_destroy();
+            }</br>
+            $_SESSION['time_session'] = time();
+        </p>
+
+    </section>
+
+
+</body>
+
+</html>
---
+++ script.php
@@ -0,0 +1,55 @@
+<?php 
+
+// ini_set('session.gc_maxlifetime', 10);
+// ini_set("session.cookie_lifetime", 10);
+// setcookie(session_name(), '', time() - 256, '/');
+// session_start();
+
+$inactive = 256; 
+ini_set('session.gc_maxlifetime', $inactive); 
+
+session_start();
+
+if (isset($_SESSION['time_session']) && (time() - $_SESSION['time_session'] > $inactive)) {
+   
+    session_unset();     
+    session_destroy();   
+}
+$_SESSION['time_session'] = time(); 
+
+if (isset($_POST['load'])) {
+
+// try {
+
+    if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_FILES['img_session'])) {
+      
+        $imageType = mime_content_type($_FILES['img_session']['tmp_name']);
+        if (strpos($imageType, 'image/') === 0) {
+           
+            $imageData = base64_encode(file_get_contents($_FILES['img_session']['tmp_name']));
+            $_SESSION['img_session'] = "data:$imageType;base64,$imageData";}
+
+            header("Refresh:0");
+}
+// } catch (Exception $e) {
+//     echo "erreur";
+// }
+}
+
+if (isset($_POST['reset'])) {
+
+    unset($_SESSION['img_session']);
+    header("Refresh:0");
+
+    
+}
+if (isset($_POST['refresh'])) {
+    header("Refresh:0");
+}
+
+
+if(isset($_SESSION['img_session'])) {
+    $img = $_SESSION['img_session'];
+} else {
+    $img = 'img.jpg';
+}
